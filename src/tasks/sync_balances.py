0dec8df1ec19c2b198a5e4ce5471dcdec3431378
Михаил <mxoilslukin@gmail.com>
2025-03-17 22:10:10 +0500

    CARD-60: small fixes

--- src/tasks/sync_balances.py
+++ src/tasks/sync_balances.py
@@ -4,6 +4,7 @@ from celery import shared_task
 from helpers.depends.db_session import get_db_session_context
 
 from src.tasks.db_client import make_db_client
+from src.web.depends.integrations import get_users_kafka
 from src.web.depends.repositories import get_user_balance_repository
 from src.web.depends.services import get_user_balance_service
 
@@ -15,5 +16,8 @@ def sync_balances_task():
 
 async def run_async_task() -> None:
     async with get_db_session_context(make_db_client()) as session:
-        service = get_user_balance_service(user_balance_repository=await get_user_balance_repository(session=session))
+        service = await get_user_balance_service(
+            user_balance_repository=await get_user_balance_repository(session=session),
+            users_kafka=await get_users_kafka(),
+        )
         await service.sync_balances()
